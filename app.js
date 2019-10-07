88d94064a7f15a5f1b4cd2597560dfa1f08d3631
George <neverender24@gmail.com>
2019-10-07 13:46:43 +0800

    2nd commit

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-jQuery(document).ready(function() {
-    jQuery('#go').click(function(){
+jQuery(document).ready(function () {
+    jQuery('#go').click(function () {
         var data = {
-            action : 'my_action',
+            action: 'my_action',
             group_name: jQuery('#group_name').val(),
-            command: 'usage-breakdown'
         }
 
         self = this
-    
-        jQuery.post(ajaxurl, data, function(response) {
+
+        jQuery.post(ajaxurl, data, function (response) {
             alert('Saved!')
             alert('Group Count Now is ' + response)
         })
--- index.php
+++ index.php
@@ -10,41 +10,45 @@
 wp_enqueue_script('mainjs', plugins_url('app.js', __FILE__));
 
 add_action('admin_menu', 'sample_admin_menu');
-function sample_admin_menu() {
-  add_menu_page('Add Group', 'Group Plugin', 'manage_options', 'group-plugin', 'group_form');
+function sample_admin_menu()
+{
+    add_menu_page('Add Group', 'Group Plugin', 'manage_options', 'group-plugin', 'group_form');
 }
 
-function group_form() {
-  echo "
-      <h3>Create group</h3>
-      <form onsubmit='event.preventDefault();'>
-        <input type='text' id='group_name' />
-        <button id='go'>Go</button>
-      </form>
-  ";
+function group_form()
+{?>
+
+    <h3>Create group</h3>
+    <form onsubmit='event.preventDefault();'>
+      <input type='text' id='group_name' />
+      <button id='go'>Go</button>
+    </form>
+
+<?php
 }
 
 add_action('wp_ajax_my_action', 'my_action');
-function my_action() {
+function my_action()
+{
     store();
-    count_group();
-
-  wp_die();
+    echo count_group();
+    wp_die();
 }
 
-function store() {
-  global $wpdb;
-  $wpdb->insert(
-    'wp_groups',
-    array(
-      'name' => $_POST['group_name'],
-    )
+function store()
+{
+    global $wpdb;
+    $wpdb->insert(
+        'wp_groups',
+        array(
+            'name' => $_POST['group_name'],
+        )
     );
 }
 
-function count_group() {
-  global $wpdb;
-  $group_count = $wpdb->get_var("SELECT COUNT('id') FROM wp_groups");
-  return $group_count;
+function count_group()
+{
+    global $wpdb;
+    $group_count = $wpdb->get_var("SELECT COUNT('id') FROM wp_groups");
+    return $group_count;
 }
-
